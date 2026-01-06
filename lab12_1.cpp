dc225ab715cf252ce3d15ccceb8b3f589a69d0ad
Namphet <namphetnanaphet@gmail.com>
2026-01-06 13:50:28 +0700

    lab12_1.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+include<iostream>
 using namespace std;
 
 int fibonacci(int);
@@ -14,5 +14,4 @@ int fibonacci(int x){
     if (x == 0) return 0;
     if (x == 1) return 1;
     return fibonacci(x - 1) + fibonacci(x - 2);
-}turn 0; 
 }
